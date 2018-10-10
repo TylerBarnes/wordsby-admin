006622270f4bbaa4d03e63cfedaefe82038f5d43
Tyler Barnes <tylerdbarnes@gmail.com>
2018-10-09 20:57:31 -0700

    remove gatsby options from build hook function file

--- functions/build-hook.php
+++ functions/build-hook.php
@@ -11,48 +11,4 @@ function build_hook($post_id)
 		   $response = Requests::post( $buildHook );
 	}
 }
-
-if( function_exists('acf_add_local_field_group') ):
-
-acf_add_local_field_group(array(
-	'key' => 'group_5bbd6668c1ee0',
-	'title' => 'Gatsby Options',
-	'fields' => array(
-		array(
-			'key' => 'field_5bbd666ed62e4',
-			'label' => 'Build Webhook',
-			'name' => 'build_webhook',
-			'type' => 'url',
-			'instructions' => '',
-			'required' => 0,
-			'conditional_logic' => 0,
-			'wrapper' => array(
-				'width' => '',
-				'class' => '',
-				'id' => '',
-			),
-			'default_value' => '',
-			'placeholder' => '',
-		),
-	),
-	'location' => array(
-		array(
-			array(
-				'param' => 'options_page',
-				'operator' => '==',
-				'value' => 'acf-options-gatsby',
-			),
-		),
-	),
-	'menu_order' => 0,
-	'position' => 'normal',
-	'style' => 'default',
-	'label_placement' => 'top',
-	'instruction_placement' => 'label',
-	'hide_on_screen' => '',
-	'active' => 1,
-	'description' => '',
-));
-
-endif;
  ?>
