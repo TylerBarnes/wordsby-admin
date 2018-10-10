0a59ccbddf717aca38a11229485cafcef6a81af7
Tyler Barnes <tylerdbarnes@gmail.com>
2018-10-09 18:35:01 -0700

    nullify false acf fields

--- functions.php
+++ functions.php
@@ -2,4 +2,5 @@
 require_once dirname( __FILE__ ) . "/lib/class-tgm-plugin-activation.php";
 require_once dirname( __FILE__ ) . "/functions/require-plugins.php";
 require_once dirname( __FILE__ ) . "/functions/discourage-search-engines.php";
+require_once dirname( __FILE__ ) . "/functions/acf-nullify-empty.php";
 ?>
