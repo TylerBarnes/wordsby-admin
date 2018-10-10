b147b62de2c7ed34fb6568c9e11a3bec87a35280
Tyler Barnes <tylerdbarnes@gmail.com>
2018-10-09 19:51:49 -0700

    add gatsby options page

--- functions.php
+++ functions.php
@@ -4,4 +4,5 @@ require_once dirname( __FILE__ ) . "/functions/require-plugins.php";
 require_once dirname( __FILE__ ) . "/functions/discourage-search-engines.php";
 require_once dirname( __FILE__ ) . "/functions/acf-nullify-empty.php";
 require_once dirname( __FILE__ ) . "/functions/always-redirect-to-admin.php";
+require_once dirname( __FILE__ ) . "/functions/main-options-page.php";
 ?>
---
+++ functions/main-options-page.php
@@ -0,0 +1,11 @@
+<?php
+
+if( function_exists('acf_add_options_page') ) {
+
+	acf_add_options_page([
+		'page_title' => 'Gatsby',
+		'icon_url' => '/wp-content/themes/gatsby-wordpress-admin-theme/assets/icons/gatsby.svg'		
+    ]);
+	
+}
+?>
