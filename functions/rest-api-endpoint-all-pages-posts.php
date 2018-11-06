9babdb73f79a7043d17f6f0a9f7038a1f9e96c5f
Tyler Barnes <tylerdbarnes@gmail.com>
2018-11-06 11:42:03 -0800

    change endpoint, return all posts, and remove paging and sticky post filtering

--- functions/rest-api-endpoint-all-pages-posts.php
+++ functions/rest-api-endpoint-all-pages-posts.php
@@ -2,7 +2,7 @@
 add_action( 'rest_api_init', 'custom_api_get_all_posts' );   
 
 function custom_api_get_all_posts() {
-    register_rest_route( 'custom/v1', '/collections', array(
+    register_rest_route( 'wp/v1', '/collections', array(
         'methods' => 'GET',
         'callback' => 'custom_api_get_all_posts_callback'
     ));
@@ -11,15 +11,11 @@ function custom_api_get_all_posts() {
 function custom_api_get_all_posts_callback( $request ) {
     // Initialize the array that will receive the posts' data. 
     $posts_data = array();
-    // Receive and set the page parameter from the $request for pagination purposes
-    $paged = $request->get_param( 'page' );
-    $paged = ( isset( $paged ) || ! ( empty( $paged ) ) ) ? $paged : 1; 
+
     // Get the posts using the 'post' and 'news' post types
     $posts = get_posts( array(
             'post_type' => 'any',
-            'paged' => $paged,
-            'post__not_in' => get_option( 'sticky_posts' ),
-            'posts_per_page' => 10,            
+            'posts_per_page' => -1,            
         )
     ); 
     // Loop through the posts and push the desired data to the array we've initialized earlier in the form of an object
@@ -27,11 +23,12 @@ function custom_api_get_all_posts_callback( $request ) {
         // write_log((array) $post);
         $id = $post->ID; 
         $post_thumbnail = ( has_post_thumbnail( $id ) ) ? get_the_post_thumbnail_url( $id ) : null;
-        $acf = get_fields($id);
+        $permalink = get_permalink($id);
 
-        $post->pathname = str_replace(home_url(), '', get_permalink($id)); 
+        $post->pathname = str_replace(home_url(), '', $permalink); 
+        $post->permalink = $permalink;
         $post->featured_img = $post_thumbnail;
-        $post->acf = $acf;
+        $post->acf = get_fields($id);
 
         $posts_data[] = $post;
     }                  
