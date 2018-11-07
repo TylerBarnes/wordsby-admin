a4a1bc48a5afd67a36f14bf04eb03379fe67d26b
Tyler Barnes <tylerdbarnes@gmail.com>
2018-11-07 09:53:38 -0800

    set template dropdown selected to selected in dom

--- js/preview.js
+++ js/preview.js
@@ -1,20 +1,25 @@
 // check this github issue for the php preview endpoint implementation
 // https://github.com/WP-API/WP-API/issues/2624
 
-// (function($) {
-//   $(document).ready(function() {
-//     var previewButton = $(".preview.button");
-//     var previewLink = previewButton.attr("href");
+(function($) {
+  $(document).ready(function() {
+    //     var previewButton = $(".preview.button");
+    //     var previewLink = previewButton.attr("href");
 
-//     previewButton.remove();
+    //     previewButton.remove();
 
-//     var previewContainer = $("#preview-action");
+    //     var previewContainer = $("#preview-action");
 
-//     previewContainer.append(`
-//     <button class="button" data-href="${previewLink}">Preview</button>`);
+    //     previewContainer.append(`
+    //     <button class="button" data-href="${previewLink}">Preview</button>`);
 
-//     $(previewButton).on("click", function(e) {
-//       e.preventDefault();
-//     });
-//   });
-// })(jQuery);
+    //     $(previewButton).on("click", function(e) {
+    //       e.preventDefault();
+    //     });
+    $("#page_template").on("change", function() {
+      var selected = $("#page_template").find(":selected");
+      $("#page_template option").removeAttr("selected");
+      $(selected).attr("selected", "selected");
+    });
+  });
+})(jQuery);
