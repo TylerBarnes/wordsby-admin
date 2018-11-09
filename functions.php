2eea44aebb5d79ac7d7f6dd02e989e7a9116d098
Tyler Barnes <tylerdbarnes@gmail.com>
2018-11-08 18:52:32 -0800

    add the function to the site

--- functions.php
+++ functions.php
@@ -3,6 +3,7 @@ require_once dirname( __FILE__ ) . "/lib/class-tgm-plugin-activation.php";
 require_once dirname( __FILE__ ) . "/functions/require-plugins.php";
 require_once dirname( __FILE__ ) . "/functions/rest-api-endpoint-all-pages-posts.php";
 require_once dirname( __FILE__ ) . "/functions/write_log.php";
+require_once dirname( __FILE__ ) . "/functions/update-nonce-cookie.php";
 require_once dirname( __FILE__ ) . "/functions/discourage-search-engines.php";
 require_once dirname( __FILE__ ) . "/functions/acf-nullify-empty.php";
 require_once dirname( __FILE__ ) . "/functions/redirect-index-to-admin.php";
