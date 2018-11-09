2fe37b95b4a2012620e0aedd45aa29f27503e44d
Tyler Barnes <tylerdbarnes@gmail.com>
2018-11-08 20:28:41 -0800

    remove all methods of getting templates except for from json via the previews folder.

--- functions/set-templates.php
+++ functions/set-templates.php
@@ -1,137 +1,36 @@
 <?php 
 
-// This was supposed to be used to return the templates to our template dropdown automatically. It works but when you preview the field is still null...
-// function acf_load_template_select( $field ) {
-    
-//     // reset choices
-//     $field['choices'] = array();
-    
-    
-//     // // get the textarea value from options page without any formatting
-//     // $choices = get_field('my_select_values', 'option', false);
-
-    
-//     // // explode the value so that each line is a new array piece
-//     // $choices = explode("\n", $choices);
-
-    
-//     // // remove any unwanted white space
-//     // $choices = array_map('trim', $choices);
-
-    
-//     // // loop through array and add to field 'choices'
-//     // if( is_array($choices) ) {
-        
-//     //     foreach( $choices as $choice ) {
-            
-//     //         $field['choices'][ $choice ] = $choice;
-            
-//     //     }
-        
-//     // }
-    
-
-//     // // return the field
-//     // return $field;
-
-//     $gatsby_templates = set_templates([]);
-
-//     $field['choices']['default'] = "Default Template";
-//     foreach($gatsby_templates as $key => $template) {
-//         $field['choices'][$$key] = $template;
-//     }
-
-//     return $field;
-    
-// }
-
-// add_filter('acf/load_field/name=template', 'acf_load_template_select');
-
 function set_templates($templates) {
-    $template_population_method = get_field('how_should_we_populate_the_template_dropdown', 'options');
-
-    if ($template_population_method === 'gatsby') {
-        return populate_templates_from_gatsby_filesystem(
-            get_field('gatsby_templates_path', 'option')
-        );
-    } elseif ($template_population_method === 'repeater') {
-        return populate_templates_from_acf_options(
-            get_field('templates', 'option')
-        );
-    } elseif(
-        $template_population_method === 'gatsby_repo' 
-        && get_field('gatsby_repo_templates_path', 'option')
-        ) {
-        return populate_templates_from_gatsby_repo(
-            get_field('gatsby_repo_templates_path', 'option')
-        );
-    } else {
-        return $templates;
-    }
-    
+    return populate_templates_from_json_file();
 }
 
 add_filter('theme_templates', 'set_templates');
 
-function populate_templates_from_gatsby_repo($template_path_from_gatsby_root) {
-    if (file_exists(get_template_directory() . "/gatsby/")) {
-        return populate_templates_from_gatsby_filesystem("gatsby/$template_path_from_gatsby_root");
-    }
-};
+function populate_templates_from_json_file() {
+    $wproot = get_home_path();
+    $templates_json_path = "$wproot/preview/templates.json";
+    $templates_json = false;
+    $templates = array();
 
-function populate_templates_from_gatsby_filesystem($gatsby_templates_path) {
-    $gatsby_templates = [];
-    $full_gatsby_templates_path = get_template_directory() . "/" . $gatsby_templates_path;
+    if (file_exists($templates_json_path)) {
+        $templates_json_str = file_get_contents($templates_json_path);
+        $templates_json = json_decode($templates_json_str);
+        $default_template_match = '/single/';
 
-    if (!file_exists($full_gatsby_templates_path)) return write_log("The specified templates directory does not exist. Trying at $full_gatsby_templates_path");
-
-    // recursively return folders and templates in an array called $r.
-    $ritit = new RecursiveIteratorIterator(new RecursiveDirectoryIterator($full_gatsby_templates_path), RecursiveIteratorIterator::CHILD_FIRST); 
-    $r = array(); 
-    foreach ($ritit as $splFileInfo) { 
-        // if it's a hidden file or a . or a .. then just skip it.
-        if (preg_match('/^(\.\w+|\.$|\.\.$)/i', $splFileInfo->getFileName())) continue;
-
-        $path = $splFileInfo->isDir() 
-                ? array($splFileInfo->getFilename() => array()) 
-                : array($splFileInfo->getFilename()); 
-
-        for ($depth = $ritit->getDepth() - 1; $depth >= 0; $depth--) { 
-            $path = array($ritit->getSubIterator($depth)->current()->getFilename() => $path); 
-        } 
-        
-        $r = array_merge_recursive($r, $path);
-    } 
-
-    // $r is an array of our folders and files. 
-    // add our files and paths to the gatsby templates array
-    foreach($r as $key => $value) {
-        if (is_string($value)) {
-            $gatsby_templates[$value] = filenameToTitle($value);
-        } elseif (is_array($value)) {
-            foreach ($value as $file) {
-                if (!is_string($file)) continue;
-
-                $gatsby_templates["$key/$file"] = filenameToTitle($file);
+        foreach($templates_json as $key => $template) {
+            $match = preg_match($default_template_match, $template);
+            if ($match) {
+                unset($templates_json[$key]);
             }
+        }
+
+        foreach($templates_json as $template) {
+            $templates[$template] = filenameToTitle($template);
         }
     }
 
-    return $gatsby_templates;
+    return $templates;
 }
-
-function populate_templates_from_acf_options($acf_templates) {
-    $gatsby_templates = [];
-
-    foreach($acf_templates as $template) {
-        $name = $template['template_name'];
-        $filename = $template['template_filename'];
-        $gatsby_templates["$filename"] = $name;
-    }
-
-    return $gatsby_templates;
-}
-
 
 // helper functions
 function fromCamelCase($camelCaseString) {
@@ -145,18 +44,10 @@ function filenameToTitle($string) {
         return write_log('Hmm, a string shouldve been passed in here..');
     }
 
-    // $fileExtension = pathinfo($string, PATHINFO_EXTENSION);
-    // $fileExtensionString = (string)$fileExtension;
-    // write_log($fileExtensionString);
-
-    // if ($fileExtensionString !== 'js') {
-    //     // write_log("$string is not a javascript file");
-    //     return false;
-    // }
-
     $without_extension = pathinfo($string, PATHINFO_FILENAME);
     $spaced_decamelcased_title = fromCamelCase($without_extension);
-    $capitalized_title = ucfirst($spaced_decamelcased_title);
+    $dashes_to_spaces = str_replace("-"," ", $spaced_decamelcased_title);
+    $capitalized_title = ucwords($dashes_to_spaces);
     
     return $capitalized_title;
 }
