28209e43ed8475f0696167386945c6b9dd7c77ef
Tyler Barnes <tylerdbarnes@gmail.com>
2018-11-09 21:27:55 -0800

    remove extra wp images + keep jpeg quality high

--- functions.php
+++ functions.php
@@ -15,7 +15,7 @@ require_once dirname( __FILE__ ) . "/functions/fix-edit-page-permalinks.php";
 require_once dirname( __FILE__ ) . "/functions/menus.php";
 require_once dirname( __FILE__ ) . "/functions/change-post-preview-link.php";
 require_once dirname( __FILE__ ) . "/functions/rest-api-preview-endpoint.php";
-require_once dirname( __FILE__ ) . "/functions/admin-scripts.php";
+require_once dirname( __FILE__ ) . "/functions/image-settings.php";
 require_once dirname( __FILE__ ) . "/functions/copy-preview-uploader-to-root.php";
 require_once dirname( __FILE__ ) . "/lib/PsychicWindow/psychic-window-posttype.php";
 ?>
---
+++ functions/image-settings.php
@@ -0,0 +1,29 @@
+<?php 
+// dont compress jpegs
+add_filter('jpeg_quality', function() {
+    return 100;
+});
+
+/**
+ * Remove standard image sizes so that these sizes are not
+ * created during the Media Upload process
+ *
+ * Tested with WP 3.2.1
+ *
+ * Hooked to intermediate_image_sizes_advanced filter
+ * See wp_generate_attachment_metadata( $attachment_id, $file ) in wp-admin/includes/image.php
+ *
+ * @param $sizes, array of default and added image sizes
+ * @return $sizes, modified array of image sizes
+ * @author Ade Walker http://www.studiograsshopper.ch
+ */
+function sgr_filter_image_sizes( $sizes) {
+		
+	unset( $sizes['thumbnail']);
+	unset( $sizes['medium']);
+	unset( $sizes['large']);
+	
+	return $sizes;
+}
+add_filter('intermediate_image_sizes_advanced', 'sgr_filter_image_sizes');
+?>
