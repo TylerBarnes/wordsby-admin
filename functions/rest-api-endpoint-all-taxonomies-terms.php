e811c5bec8bdffd882968a9db7da52e6ba20067a
Tyler Barnes <tylerdbarnes@gmail.com>
2018-11-26 18:33:06 -0800

    update new taxonomy terms endpoint

--- functions/rest-api-endpoint-all-taxonomies-terms.php
+++ functions/rest-api-endpoint-all-taxonomies-terms.php
@@ -2,7 +2,7 @@
 add_action( 'rest_api_init', 'custom_api_get_all_taxonomies_terms' );   
 
 function custom_api_get_all_taxonomies_terms() {
-    register_rest_route( 'wp/v1', '/taxonomies-terms', array(
+    register_rest_route( 'wp/v1', '/tax-terms', array(
         'methods' => 'GET',
         'callback' => 'custom_api_get_all_taxonomies_terms_callback'
     ));
@@ -20,24 +20,31 @@ function custom_api_get_all_taxonomies_terms_callback() {
     $site_url = get_site_url();
 
     foreach ($taxonomies as $taxonomy) {
-        $taxonomies_terms[$taxonomy] = [
+        $taxonomy_details = get_taxonomy($taxonomy);
+
+        $tax_term = [
+            'name' => $taxonomy_details->name,
+            'id' => $taxonomy_details->name,
+            'label' => $taxonomy_details->label,
             'pathname' => get_taxonomy_archive_link($taxonomy),
             'terms' => []
         ];
         $terms = get_terms($taxonomy);
-        if (count($terms) === 0) $taxonomies_terms[$taxonomy]['terms']['dummy'] = null;
+        // if (count($terms) === 0) $tax_term['terms']['dummy'] = null;
 
         foreach ($terms as $term) {
             $term_link = get_term_link($term);
             $pathname = $term_link ? str_replace($site_url, '', $term_link) : null;
-            $taxonomies_terms[$taxonomy]['terms'][$term->slug] = [
+            $tax_term['terms'][] = [
                 'slug' => $term->slug,
                 'name' => $term->name,
-                'id' => $term->term_id,
+                'wordpress_id' => $term->term_id,
                 'taxonomy' => $term->taxonomy,
                 'pathname' => $pathname
             ];
         }
+
+        $taxonomies_terms[] = $tax_term;
     }
 
     return $taxonomies_terms;
