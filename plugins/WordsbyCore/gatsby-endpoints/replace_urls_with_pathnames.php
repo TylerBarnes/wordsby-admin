b4d122dfe735a84544df6de10d6dcf6134b16e3f
Tyler Barnes <tylerdbarnes@gmail.com>
2018-11-28 09:23:05 -0800

    only replace strings

--- plugins/WordsbyCore/gatsby-endpoints/replace_urls_with_pathnames.php
+++ plugins/WordsbyCore/gatsby-endpoints/replace_urls_with_pathnames.php
@@ -3,9 +3,13 @@ function remove_urls(&$item, $key) {
     $item = replace_urls_with_pathnames($item);
 }
 
-function replace_urls_with_pathnames($text) {
-    $url = preg_quote(get_site_url(), "/");
-    $match = "/$url(?!\/wp-content\/|\/wp-admin\/|\/wp-includes\/)/";
-    return preg_replace($match, '', $text);
+function replace_urls_with_pathnames($input) {
+    if (is_string($input)) {
+        $url = preg_quote(get_site_url(), "/");
+        $match = "/$url(?!\/wp-content\/|\/wp-admin\/|\/wp-includes\/)/";
+        return preg_replace($match, '', $input);
+    } else {
+        return $input;
+    }
 }
 ?>
