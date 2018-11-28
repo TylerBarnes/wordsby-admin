3c60d4633a57d68db7ba6771016fc296e434814d
Robin <robin@bare.ca>
2018-11-28 13:01:10 -0800

    Revert "Add ACF google maps api key"
    This reverts commit b1c0c84e0841bb55d3e4616f06761da706d4871b.

--- functions.php
+++ functions.php
@@ -4,14 +4,19 @@ require_once dirname( __FILE__ ) . "/functions/require-plugins.php";
 require_once dirname( __FILE__ ) . "/functions/rest-api-endpoint-all-pages-posts.php";
 require_once dirname( __FILE__ ) . "/functions/write_log.php";
 require_once dirname( __FILE__ ) . "/functions/populate-templates-from-json-file.php";
+
 require_once dirname( __FILE__ ) . "/functions/acf-nullify-empty.php";
+
 require_once dirname( __FILE__ ) . "/functions/discourage-search-engines.php";
 require_once dirname( __FILE__ ) . "/functions/redirect-index-to-admin.php";
+
 require_once dirname( __FILE__ ) . "/functions/main-options-page.php";
+
 require_once dirname( __FILE__ ) . "/functions/menus.php";
+
 require_once dirname( __FILE__ ) . "/functions/always-one-post-one-term.php";
+
 require_once dirname( __FILE__ ) . "/functions/acf-fields.php";
-require_once dirname( __FILE__ ) . "/functions/googleMapKey.php";
 
 // previews / editing
 require_once dirname( __FILE__ ) . "/functions/update-nonce-cookie.php";
@@ -24,10 +29,14 @@ require_once dirname( __FILE__ ) . "/lib/GatsbyPreviews/receivePreviews.php";
 // end previews
 
 require_once dirname( __FILE__ ) . "/functions/image-settings.php";
+
 require_once dirname( __FILE__ ) . "/functions/admin-menu.php";
 require_once dirname( __FILE__ ) . "/functions/replace-admin-bar.php";
 require_once dirname( __FILE__ ) . "/functions/replace-wp-dashboard.php";
+
 require_once dirname( __FILE__ ) . "/lib/class-download-remote-image.php"; 
 require_once dirname( __FILE__ ) . "/functions/random-unsplash-avatars.php";
+
+
 require_once dirname( __FILE__ ) . "/lib/PsychicWindow/psychic-window-posttype.php";
 ?>
