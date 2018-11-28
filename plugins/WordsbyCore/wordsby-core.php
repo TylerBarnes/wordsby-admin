e4769b68fb697ad5b416b53765325c57893bb78e
Tyler Barnes <tylerdbarnes@gmail.com>
2018-11-28 09:23:26 -0800

    add endpoints to wordsby

--- plugins/WordsbyCore/wordsby-core.php
+++ plugins/WordsbyCore/wordsby-core.php
@@ -3,4 +3,5 @@ require_once dirname( __FILE__ ) . "/netlify/netlify.php";
 require_once dirname( __FILE__ ) . "/gatsby-fixes/gatsby-fixes.php";
 require_once dirname( __FILE__ ) . "/archive-templates/archive-templates.php";
 require_once dirname( __FILE__ ) . "/previews/previews.php";
+require_once dirname( __FILE__ ) . "/gatsby-endpoints/gatsby-endpoints.php";
 ?>
