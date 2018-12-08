273015cc1b6f2bfe320a05f2e07f2a7c92c7c4ce
Tyler Barnes <tylerdbarnes@gmail.com>
2018-12-08 11:09:32 -0800

    move stylesheet to a separate file

--- plugins/BetterAdmin/better-admin.php
+++ plugins/BetterAdmin/better-admin.php
@@ -1,4 +1,8 @@
 <?php 
+require_once dirname( __FILE__ ) . "/better-admin-menu.php";
+require_once dirname( __FILE__ ) . "/better-toolbar.php";
+require_once dirname( __FILE__ ) . "/better-dashboard.php";
+
 add_action( 'admin_enqueue_scripts', 'addAdminMenuScripts' );
 function addAdminMenuScripts() {
     wp_register_script( 'menu-aim', get_stylesheet_directory_uri() . "/plugins/BetterAdmin/assets/js/jquery.menu-aim.js" );
@@ -7,11 +11,7 @@ function addAdminMenuScripts() {
     wp_register_script( 'menu', get_stylesheet_directory_uri() . "/plugins/BetterAdmin/assets/js/betteradmin-menu.js", array('menu-aim') );
     wp_enqueue_script( 'menu' );
 
+    wp_enqueue_style( 'betteradmin-style', get_stylesheet_directory_uri() . "/plugins/BetterAdmin/assets/css/style.css" );
 }
 
-
-
-require_once dirname( __FILE__ ) . "/better-admin-menu.php";
-require_once dirname( __FILE__ ) . "/better-toolbar.php";
-require_once dirname( __FILE__ ) . "/better-dashboard.php";
 ?>
