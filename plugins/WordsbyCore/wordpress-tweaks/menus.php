0d43a5f7d6d9f43eb6576c5f438fe2567f573568
Tyler Barnes <tylerdbarnes@gmail.com>
2018-12-09 14:54:22 -0800

    build menu if it doesn't exist

--- plugins/WordsbyCore/wordpress-tweaks/menus.php
+++ plugins/WordsbyCore/wordpress-tweaks/menus.php
@@ -1,3 +1,24 @@
 <?php 
 add_theme_support( 'menus' );
+
+add_action('init', 'default_menu');
+
+function default_menu() {
+    // Check if the menu exists
+    $menu_name = 'Main Menu';
+    $menu_exists = wp_get_nav_menu_object( $menu_name );
+
+    // If it doesn't exist, let's create it.
+    if( !$menu_exists){
+        $menu_id = wp_create_nav_menu($menu_name);
+
+        // Set up default menu items
+        wp_update_nav_menu_item($menu_id, 0, array(
+            'menu-item-title' =>  __('Home'),
+            'menu-item-classes' => 'home',
+            'menu-item-url' => home_url( '/' ), 
+            'menu-item-status' => 'publish'));
+
+    }
+}
 ?>
