91c3bff5383895a5794b0ccc96f92c3062736e5c
Tyler Barnes <tylerdbarnes@gmail.com>
2018-12-25 01:56:18 -0800

    update folder structure

--- plugins/Wordlify/github/github.php
+++ plugins/Wordlify/github/github.php
@@ -5,6 +5,6 @@ require_once __DIR__ . "/utils/branching.php";
 
 require_once __DIR__ . "/actions/save_post.php";
 require_once __DIR__ . "/actions/update_menus.php";
-require_once __DIR__ . "/actions/attachments.php";
+require_once __DIR__ . "/actions/attachments/wp_handle_upload.php";
 
 ?>
