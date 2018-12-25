620e4c9e73284c2085c2fd315e5929f877399ea6
Tyler Barnes <tylerdbarnes@gmail.com>
2018-12-25 00:17:02 -0800

    rename

--- plugins/GitlifyWordsby/gitlab/gitlab.php
+++ plugins/GitlifyWordsby/gitlab/gitlab.php
@@ -4,8 +4,8 @@ require_once __DIR__ . "/utils/branching.php";
 require_once __DIR__ . "/utils/setup.php";
 require_once __DIR__ . "/utils/general.php";
 
-require_once __DIR__ . "/hooks/collections.php";
-require_once __DIR__ . "/hooks/media.php";
-require_once __DIR__ . "/hooks/menus.php";
+require_once __DIR__ . "/actions/save_post.php";
+require_once __DIR__ . "/actions/attachments.php";
+require_once __DIR__ . "/actions/update_menus.php";
 
 ?>
