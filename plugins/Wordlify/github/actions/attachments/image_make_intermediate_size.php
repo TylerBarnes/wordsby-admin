d1de50f06e2cfcd8ddd9dd3b720b9bdf0bf362fb
Tyler Barnes <tylerdbarnes@gmail.com>
2018-12-27 18:25:42 -0800

    add file editing capability

--- plugins/Wordlify/github/actions/attachments/image_make_intermediate_size.php
+++ plugins/Wordlify/github/actions/attachments/image_make_intermediate_size.php
@@ -24,8 +24,6 @@ function commitEditedMedia($full_filepath) {
         return $full_filepath;
     };
 
-	$dirname = pathinfo( $full_filepath, PATHINFO_DIRNAME );
-	$ext = pathinfo( $full_filepath, PATHINFO_EXTENSION );
     $filename = pathinfo( $full_filepath, PATHINFO_FILENAME );
 
     $repo_full_filepath = "wordsby/" . substr(
@@ -33,69 +31,42 @@ function commitEditedMedia($full_filepath) {
         strpos($full_filepath, "/uploads/") + 1
     );  
 
-    $repo_filename = pathinfo( $repo_full_filepath, PATHINFO_BASENAME );
-	$repo_dirname  = pathinfo( $repo_full_filepath, PATHINFO_DIRNAME );
-
-
     $client = getGithubClient();
     if (!$client) return null; 
 
     createMediaBranchIfItDoesntExist($client);
     
-    global $mediaBranch;
-
-
-    // $site_url = get_site_url();
-    // $current_user = wp_get_current_user()->data;
-    // $username = $current_user->user_nicename;
-
-    // $commit_message = "
-    //         \"$filename\" edited (\"$repo_filename\") 
-    //         — by $username (from $site_url)
-    //     ";
-
-    // $actions = array(
-    //     array(
-    //         'action' => $action,
-    //         'file_path' => $repo_full_filepath,
-    //         'content' => base64_encode(file_get_contents($full_filepath)),
-    //         'encoding' => 'base64'
-    //     )
-    // );
-
-    // $original_filename = preg_replace( 
-    //     '/-e([0-9]+)$/', '', $filename 
-    //     ) . ".$ext";
-
-    // $repo_original_filepath = "$repo_dirname/$original_filename";
-    
-    // $original_media_exists = isFileInRepo(
-    //     $client, $repo_dirname, $original_filename, $mediaBranch
-    // );
-    
-    // // delete the original media file from the repo if 
-    // // IMAGE_EDIT_OVERWRITE is true.
+    // delete the original media file from the repo if 
+    // IMAGE_EDIT_OVERWRITE is true.
     // if (
     //     defined( 'IMAGE_EDIT_OVERWRITE' ) && 
     //     IMAGE_EDIT_OVERWRITE &&
     //     $original_media_exists
     //     ) {
-    //     array_push($actions, array(
-    //         'action' => 'delete',
-    //         'file_path' => $repo_original_filepath,
-    //     ));
+       
     // }
+    // to do the above, we'll need to create a deleteCommittedFile function
+    // or equivalent .
+    // the commit function only adds or modifies files. It doesn't remove them.
+    // alternatively we could update the commit file to delete as well..
+    // check delete_attachment.php in the github folder
+    // also check image_make_intermediate_sizes.php in the gitlab folder
+    // as the function there respects IMAGE_EDIT_OVERWRITE
 
-    // $commit = $client->api('repositories')->createCommit(
-    //     WORDLIFY_GITLAB_PROJECT_ID, 
-    //     array(
-    //         'branch' => $mediaBranch, 
-    //         'commit_message' => $commit_message,
-    //         'actions' => $actions,
-    //         'author_email' => $username,
-    //         'author_name' => $current_user->user_email
-    //     )
-    // );
+    commit(
+        createCommitMessage(
+            null, $filename, "Edited "
+        ),
+        [
+            [
+                'path' => $repo_full_filepath,
+                'content' => base64_encode(
+                    file_get_contents($full_filepath)
+                ), 
+                'encoding' => 'base64'
+            ]
+        ]
+    );
 
     return $full_filepath;
 }
